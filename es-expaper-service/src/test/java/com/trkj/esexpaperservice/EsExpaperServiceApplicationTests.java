ff5501ad900ee7b8f20d469a01921cb5c4292ee9
orealc <2806964882@qq.com>
2022-09-18 14:24:15 +0800

    9.18第二次

---
+++ es-expaper-service/src/test/java/com/trkj/esexpaperservice/EsExpaperServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trkj.esexpaperservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsExpaperServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ es-staff-service/src/main/java/com/trkj/esstaffservice/EsStaffServiceApplication.java
@@ -0,0 +1,13 @@
+package com.trkj.esstaffservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsStaffServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsStaffServiceApplication.class, args);
+    }
+
+}
