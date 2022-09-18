2518253d46f09ef801052299191490a141be7473
orealc <2806964882@qq.com>
2022-09-18 13:23:35 +0800

    9.18文鑫上传

--- Examination/src/App.vue
+++ Examination/src/App.vue
@@ -1,31 +1,32 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <!-- 头部 -->
+    <Header></Header>
+    <!-- 主体 -->
+    <div style="display: flex">
+      <!-- 侧边栏 -->
+      <Aside></Aside>
+      <!-- 内容区域 -->
+      <Mans style="flex: 1" />
+
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style>
+
 </style>
+
+<script>
+  import Aside from "./components/Aside.vue"
+  import Header from "./components/Header.vue"
+  import Mans from "./view/Mans.vue"
+  export default {
+    name: "App",
+    components: {
+      Aside,
+      Header,
+      Mans
+    }
+  }
+</script>
--- Examination/src/main.js
+++ Examination/src/main.js
@@ -1,5 +1,14 @@
-import { createApp } from 'vue'
-import './style.css'
+import {
+    createApp
+} from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import './assets/css/global.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import zhCn from 'element-plus/lib/locale/lang/zh-cn' //把默认的英文改成中文
+import router from './router'
+createApp(App).use(VueAxios, axios).use(router).use(ElementPlus, {
+    locale: zhCn,
+}).mount('#app')
